0c53ca7bd6134c906ccacde3754f286289885c62
Mostafa-Medhat <mmedhat72@gmail.com>
2023-02-09 00:45:21 +0200

    Update Timer1_PWM.c

--- Timer1_PWM.c
+++ Timer1_PWM.c
@@ -8,10 +8,12 @@
 #include "avr/io.h"
 #include "Timer1_PWM.h"
 
+unsigned long TopValue;
+
 void timer1_PWM_Init(void){
 	TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1<< WGM11);
 	TCCR1B = (1<<WGM13) | (1<<WGM12) | (1<<CS10);
-	unsigned long TopValue = ((float)100/PWM_FREQUENCY_IN_KHZ)*160;
+	TopValue = ((float)100/PWM_FREQUENCY_IN_KHZ)*160;
 	ICR1 = TopValue;
 //	OCR1A = 20000;
 //	OCR1B = 45500;
@@ -20,19 +22,19 @@ void timer1_PWM_Init(void){
 }
 
 void timer1_SetPWM_B(unsigned char dutyB){
-	unsigned long actualDuty = (unsigned long)(((float)dutyB/100)*(((float)100/PWM_FREQUENCY_IN_KHZ)*160));
+	unsigned long actualDuty = (unsigned long)(((float)dutyB/100)*(TopValue));
 	OCR1B = actualDuty;
 }
 
 
 void timer1_SetPWM_A(unsigned char dutyA){
-	unsigned long actualDuty = (unsigned long)(((float)dutyA/100)*(((float)100/PWM_FREQUENCY_IN_KHZ)*160));
+	unsigned long actualDuty = (unsigned long)(((float)dutyA/100)*(TopValue));
 	OCR1A = actualDuty;
 }
 
 
 void timer1_SetPWM_A_B(unsigned char dutyA_B){
-	unsigned long actualDuty = (unsigned long)(((float)dutyA_B/100)*(((float)100/PWM_FREQUENCY_IN_KHZ)*160));
+	unsigned long actualDuty = (unsigned long)(((float)dutyA_B/100)*(TopValue));
 	OCR1A = actualDuty;
 	OCR1B = actualDuty;
 }
