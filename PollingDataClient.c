b5da8d31e4ec3cbe4378d1a2adc195f217a8b884
Mostafa-Medhat <mmedhat72@gmail.com>
2023-02-11 20:07:02 +0200

    remove LCD

--- PollingDataClient.c
+++ PollingDataClient.c
@@ -16,50 +16,41 @@
 #define WAIT_TIME_PER_DUTY_CHANGE	30
 //uint8_t ignitionState = LOGIC_LOW;
 unsigned char state;
-
-void Handle_data(States_GroupType *Sates_Ptr)
-{
-
-	LCD_moveCursor(0, 0);
-	LCD_displayString("H:"); //hazard button
-	LCD_intgerToString(Sates_Ptr->hazard_Btn);
-	LCD_displayString(" L:");	//left indicator
-	LCD_intgerToString(Sates_Ptr->leftIndicator);
-	LCD_moveCursor(1, 0);
-	LCD_displayString(" R:");	//right indicator
-	LCD_intgerToString(Sates_Ptr->rightIndicator);
-	LCD_displayString(" I:");	//ignition button
-	LCD_intgerToString(Sates_Ptr->ignition_key);
-
-	if(Sates_Ptr->ignition_key==TRUE)
-	{
-		if(Sates_Ptr->rightIndicator==TRUE)
-		{
-			state = RIGHT_INDICATOR;
-		}
-		else if(Sates_Ptr->leftIndicator==TRUE)
-		{
-			state = LEFT_INDICATOR;
-		}
-		else{
-			state = NO_INDICATOR;
-		}
-	}
-	else{
-		state = NO_INDICATOR;
-	}
-
-	if(Sates_Ptr->hazard_Btn==TRUE)
-	{
-		state = HAZARD_BUTTON;
-	}
+unsigned char ToggleLED = 0;
 
 
+//void Handle_data(States_GroupType *Sates_Ptr)
+//{
+//
+//	if(Sates_Ptr->ignition_key==TRUE)
+//	{
+//		if(Sates_Ptr->rightIndicator==TRUE)
+//		{
+//			state = RIGHT_INDICATOR;
+//		}
+//		else if(Sates_Ptr->leftIndicator==TRUE)
+//		{
+//			state = LEFT_INDICATOR;
+//		}
+//		else{
+//			state = NO_INDICATOR;
+//		}
+//	}
+//	else{
+//		state = NO_INDICATOR;
+//	}
+//
+//	if(Sates_Ptr->hazard_Btn==TRUE)
+//	{
+//		state = HAZARD_BUTTON;
+//	}
+//
+//
+//
+////	State_Handler();
+//}
 
-	State_Handler();
-}
-
-void State_Handler(void)
+void State_Handler()
 {
 	switch(state)
 	{
@@ -75,60 +66,89 @@ void State_Handler(void)
 	case HAZARD_BUTTON:
 		BothLEDS_Blink();
 		break;
-
 	}
 
 }
 
-
-void RightLED_Blink(void)
+void RightLED_Blink()
 {
-	int i = 0;
-	timer1_SetPWM_A(0);
-	for(i = 2 ; i < 17 ; i++){
-
-		timer1_SetPWM_B((i*5));
-//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
-	}
-	for(i = 16 ; i > 1 ; i--){
-
-		timer1_SetPWM_B((i*5));
-//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
-	}
+//	xTimerReset(xTimer1);
+	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, ToggleLED);
+	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, LOGIC_LOW);
 }
 
-void LeftLED_Blink(void)
+void LeftLED_Blink()
 {
-	int i = 0;
-	timer1_SetPWM_B(0);
-	for(i = 2 ; i < 17 ; i++){
-		timer1_SetPWM_A((i*5));
+//	xTimerReset(xTimer1);
 
-//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
-	}
-	for(i = 16 ; i > 1 ; i--){
-		timer1_SetPWM_A((i*5));
+	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, ToggleLED);
+	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, LOGIC_LOW);
 
-//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
-	}
 }
 
-void BothLEDS_Blink(void)
+void NoLED_Blink(void)
 {
-	int i = 0;
-	for(i = 2 ; i < 17 ; i++){
-		timer1_SetPWM_A_B(i*5);
-//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
-	}
-	for(i = 16 ; i > 1 ; i--){
-		timer1_SetPWM_A_B(i*5);
-//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
-	}
 
+	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, LOGIC_LOW);
+	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, LOGIC_LOW);
 }
 
-
-void NoLED_Blink(void){
-	timer1_SetPWM_A_B(0);
+void BothLEDS_Blink()
+{
+//	xTimerReset(xTimer1);
+	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, ToggleLED);
+	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, ToggleLED);
 }
 
+//
+//void RightLED_Blink(void)
+//{
+//	int i = 0;
+//	timer1_SetPWM_A(0);
+//	for(i = 2 ; i < 17 ; i++){
+//
+//		timer1_SetPWM_B((i*5));
+//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
+//	}
+//	for(i = 16 ; i > 1 ; i--){
+//
+//		timer1_SetPWM_B((i*5));
+//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
+//	}
+//}
+//
+//void LeftLED_Blink(void)
+//{
+//	int i = 0;
+//	timer1_SetPWM_B(0);
+//	for(i = 2 ; i < 17 ; i++){
+//		timer1_SetPWM_A((i*5));
+//
+//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
+//	}
+//	for(i = 16 ; i > 1 ; i--){
+//		timer1_SetPWM_A((i*5));
+//
+//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
+//	}
+//}
+//
+//void BothLEDS_Blink(void)
+//{
+//	int i = 0;
+//	for(i = 2 ; i < 17 ; i++){
+//		timer1_SetPWM_A_B(i*5);
+//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
+//	}
+//	for(i = 16 ; i > 1 ; i--){
+//		timer1_SetPWM_A_B(i*5);
+//		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
+//	}
+//
+//}
+//
+//
+//void NoLED_Blink(void){
+//	timer1_SetPWM_A_B(0);
+//}
+//
--- PollingDataClient.h
+++ PollingDataClient.h
@@ -7,17 +7,20 @@
 #define INC_POLLINGDATACLIENT_H_
 #include "device.h"
 
+
 #define IGNITION_STATE	0
 #define RIGHT_INDICATOR	1
 #define LEFT_INDICATOR	2
 #define NO_INDICATOR	3
 #define HAZARD_BUTTON	4
 
-void Handle_data(States_GroupType *Sates_Ptr);
-void State_Handler(void);
-void RightLED_Blink(void);
-void LeftLED_Blink(void);
-void BothLEDS_Blink(void);
+extern unsigned char ToggleLED;
+
+//void Handle_data(States_GroupType *Sates_Ptr);
+void State_Handler();
+void RightLED_Blink();
+void LeftLED_Blink();
+void BothLEDS_Blink();
 void NoLED_Blink(void);
 
 
