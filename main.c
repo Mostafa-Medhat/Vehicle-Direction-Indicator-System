2428d476f584b1216aed5b3680d3eacef2d69fb4
Mostafa-Medhat <mmedhat72@gmail.com>
2023-02-11 21:14:05 +0200

    HandleData is ON

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ void Task4_PWM(void);
 int main(void)
 {
 	DIO_init();
-	timer1_PWM_Init();
+//	timer1_PWM_Init();
 
 	SREG |=(1<<7);			// I bit Enabled
 
@@ -43,18 +43,16 @@ int main(void)
 
 	TimerHandle_t xTimer1 = xTimerCreate("timer1",10/portTICK_PERIOD_MS,pdTRUE,0,Task1_GetData);
 	TimerHandle_t xTimer2 = xTimerCreate("timer2",500/portTICK_PERIOD_MS,pdTRUE,0,Task3_ToggleLED);
-	TimerHandle_t xTimer3 = xTimerCreate("timer3",30/portTICK_PERIOD_MS,pdTRUE,0,Task4_PWM);
+//	TimerHandle_t xTimer3 = xTimerCreate("timer3",30/portTICK_PERIOD_MS,pdTRUE,0,Task4_PWM);
 
 
+	xTimerStart(xTimer1, 1);
 	xTimerStart(xTimer2, 1);
-	xTimerStart(xTimer1, 10);
-	xTimerStart(xTimer3, 10);
-
-
-
+//	xTimerStart(xTimer3, 10);
 
 	vTaskStartScheduler();
-	while(1){
+	while(1)
+	{
 
 	}
 }
@@ -66,49 +64,19 @@ void Task1_GetData(void)
 	ButtonStateTest = Get_Data();
 }
 
+void Task2_HandleData(void)
+{
+	while(1)
+	{
+		Handle_data(&ButtonStateTest);
+	}
+}
+
+
 void Task3_ToggleLED(void)
 {
 	ToggleLED = !ToggleLED;
 }
-
-
-
-
-/*******************************Some unworking tests**************************************************************/
-void Task2_HandleData(void){
-	while(1)
-	{
-
-		if(ButtonStateTest.ignition_key==TRUE)
-			{
-				if(ButtonStateTest.rightIndicator==TRUE)
-				{
-					state = RIGHT_INDICATOR;
-				}
-				else if(ButtonStateTest.leftIndicator==TRUE)
-				{
-					state = LEFT_INDICATOR;
-				}
-				else
-				{
-					state = NO_INDICATOR;
-				}
-			}
-			else{
-				state = NO_INDICATOR;
-			}
-
-			if(ButtonStateTest.hazard_Btn==TRUE)
-			{
-				state = HAZARD_BUTTON;
-			}
-
-		State_Handler();
-
-//		Handle_data(&ButtonStateTest);
-	}
-}
-
 
 
 void Task4_PWM(void)
