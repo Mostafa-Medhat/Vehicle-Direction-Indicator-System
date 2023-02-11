1d24c3bd2886b9e84c4778c19bed06f497ef3331
Mostafa-Medhat <mmedhat72@gmail.com>
2023-02-11 21:13:35 +0200

    Update PollingDataClient.c

--- PollingDataClient.c
+++ PollingDataClient.c
@@ -20,36 +20,36 @@ unsigned char RightLED_counter = 0;
 
 
 
-//void Handle_data(States_GroupType *Sates_Ptr)
-//{
-//
-//	if(Sates_Ptr->ignition_key==TRUE)
-//	{
-//		if(Sates_Ptr->rightIndicator==TRUE)
-//		{
-//			state = RIGHT_INDICATOR;
-//		}
-//		else if(Sates_Ptr->leftIndicator==TRUE)
-//		{
-//			state = LEFT_INDICATOR;
-//		}
-//		else{
-//			state = NO_INDICATOR;
-//		}
-//	}
-//	else{
-//		state = NO_INDICATOR;
-//	}
-//
-//	if(Sates_Ptr->hazard_Btn==TRUE)
-//	{
-//		state = HAZARD_BUTTON;
-//	}
-//
-//
-//
-////	State_Handler();
-//}
+void Handle_data(States_GroupType *Sates_Ptr)
+{
+
+	if(Sates_Ptr->ignition_key==TRUE)
+	{
+		if(Sates_Ptr->rightIndicator==TRUE)
+		{
+			state = RIGHT_INDICATOR;
+		}
+		else if(Sates_Ptr->leftIndicator==TRUE)
+		{
+			state = LEFT_INDICATOR;
+		}
+		else{
+			state = NO_INDICATOR;
+		}
+	}
+	else{
+		state = NO_INDICATOR;
+	}
+
+	if(Sates_Ptr->hazard_Btn==TRUE)
+	{
+		state = HAZARD_BUTTON;
+	}
+
+
+
+	State_Handler();
+}
 
 void State_Handler()
 {
@@ -73,107 +73,81 @@ void State_Handler()
 
 }
 
-//void RightLED_Blink()
-//{
-////	xTimerReset(xTimer1);
-//	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, ToggleLED);
-//	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, LOGIC_LOW);
-//}
-//
-//void LeftLED_Blink()
-//{
-////	xTimerReset(xTimer1);
-//
-//	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, ToggleLED);
-//	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, LOGIC_LOW);
-//
-//}
-//
-//void NoLED_Blink(void)
-//{
-//
-//	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, LOGIC_LOW);
-//	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, LOGIC_LOW);
-//}
-//
-//void BothLEDS_Blink()
-//{
-////	xTimerReset(xTimer1);
-//	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, ToggleLED);
-//	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, ToggleLED);
-//}
-
-
-
-
-
-void RightLED_Blink(void)
+void RightLED_Blink()
 {
+//	xTimerReset(xTimer1);
+	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, ToggleLED);
+	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, LOGIC_LOW);
+}
 
-//	static unsigned char rightState = 0;
-//	if (rightState == 0)
-//	{
-//		timer1_SetPWM_A((0));
-//		timer1_SetPWM_B((100));
-//		rightState = 1;
-//	}
-//	else
-//	{
-//		rightState = 0;
-//		timer1_SetPWM_B((0));
-//	}
-
-//	int i = 0;
-	timer1_SetPWM_A(0);
-//	RightLED_counter++;
-//	if(RightLED_counter%3 == 0 && ToggleLED == 1)
-//	{
-//		RightLED_counter = 0;
-//		i = 1;
-//	}
-//	else if(RightLED_counter%3 == 0 && ToggleLED == 0)
-//	{
-//		RightLED_counter = 0;
-//		i = 0;
-//	}
-	timer1_SetPWM_B((rightIntnesity*5));
+void LeftLED_Blink()
+{
+//	xTimerReset(xTimer1);
+	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, ToggleLED);
+	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, LOGIC_LOW);
 
 }
 
-
-void LeftLED_Blink(void)
+void NoLED_Blink(void)
 {
 
-//	int i = 0;
-//	timer1_SetPWM_B(50);
-//	for(i = 2 ; i < 17 ; i++){
-//		timer1_SetPWM_A((i*5));
+	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, LOGIC_LOW);
+	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, LOGIC_LOW);
+}
+
+void BothLEDS_Blink()
+{
+//	xTimerReset(xTimer1);
+	GPIO_writePin(dio_config_array[RIGHT_LED_ID].port_id, dio_config_array[RIGHT_LED_ID].pin_num, ToggleLED);
+	GPIO_writePin(dio_config_array[LEFT_LED_ID].port_id, dio_config_array[LEFT_LED_ID].pin_num, ToggleLED);
+}
+
+
+
+
 //
+//void RightLED_Blink(void)
+//{
+//
+////	timer1_SetPWM_A(0);
+////	timer1_SetPWM_B((rightIntnesity*5));
+//
+//}
+//
+//
+//void LeftLED_Blink(void)
+//{
+//
+////	int i = 0;
+////	timer1_SetPWM_B(50);
+////	for(i = 2 ; i < 17 ; i++){
+////		timer1_SetPWM_A((i*5));
+////
+////		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
+////	}
+////	for(i = 16 ; i > 1 ; i--){
+////		timer1_SetPWM_A((i*5));
+////
+////		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
+////	}
+//}
+//
+//void BothLEDS_Blink(void)
+//{
+//	int i = 0;
+//	for(i = 2 ; i < 17 ; i++){
+//		timer1_SetPWM_A_B(i*5);
 //		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
 //	}
 //	for(i = 16 ; i > 1 ; i--){
-//		timer1_SetPWM_A((i*5));
-//
+//		timer1_SetPWM_A_B(i*5);
 //		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
 //	}
-}
-
-void BothLEDS_Blink(void)
-{
-	int i = 0;
-	for(i = 2 ; i < 17 ; i++){
-		timer1_SetPWM_A_B(i*5);
-		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
-	}
-	for(i = 16 ; i > 1 ; i--){
-		timer1_SetPWM_A_B(i*5);
-		vTaskDelay(WAIT_TIME_PER_DUTY_CHANGE/portTICK_PERIOD_MS);
-	}
-
-}
-
-
-void NoLED_Blink(void){
-	timer1_SetPWM_A_B(0);
-}
 //
+//}
+//
+//
+//void NoLED_Blink(void){
+//	timer1_SetPWM_A_B(0);
+//}
+////
