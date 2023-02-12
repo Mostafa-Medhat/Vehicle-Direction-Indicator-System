64586de5e1bb0792096548b1f94a2a3a2612ad6a
Mostafa-Medhat <mmedhat72@gmail.com>
2023-02-12 21:31:58 +0200

    Build_Update

--- Debug/Source/list.d
+++ Debug/Source/list.d
@@ -1,25 +1,25 @@
 Source/list.o Source/list.o: ../Source/list.c \
-  E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/FreeRTOS.h \
-  E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/FreeRTOSConfig.h \
-  E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/projdefs.h \
-  E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/portable.h \
-  E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/deprecated_definitions.h \
-  E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\portable\GCC\ATMega323/portmacro.h \
-  E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/mpu_wrappers.h \
-  E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/list.h
+  D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/FreeRTOS.h \
+  D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/FreeRTOSConfig.h \
+  D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/projdefs.h \
+  D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/portable.h \
+  D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/deprecated_definitions.h \
+  D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\portable\GCC\ATMega323/portmacro.h \
+  D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/mpu_wrappers.h \
+  D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/list.h
 
-E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/FreeRTOS.h:
+D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/FreeRTOS.h:
 
-E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/FreeRTOSConfig.h:
+D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/FreeRTOSConfig.h:
 
-E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/projdefs.h:
+D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/projdefs.h:
 
-E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/portable.h:
+D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/portable.h:
 
-E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/deprecated_definitions.h:
+D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/deprecated_definitions.h:
 
-E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\portable\GCC\ATMega323/portmacro.h:
+D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\portable\GCC\ATMega323/portmacro.h:
 
-E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/mpu_wrappers.h:
+D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/mpu_wrappers.h:
 
-E:\4th\ year\Embedded\projects\FinalProject\Vehicle-Direction-FinalProject\Source\include/list.h:
+D:\Eclipse_V2\workspaces\AdvEmbedded_Project\Vehicle-Direction-FinalProject\Source\include/list.h:
